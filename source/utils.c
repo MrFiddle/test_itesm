6011a4c8c467427c697ab5521181bbf5850b8432
Tonathiu Morales <tonathiu.morales@continental.com>
2020-12-04 09:28:10 -0600

    Comment function.

--- source/utils.c
+++ source/utils.c
@@ -1,5 +1,11 @@
 #include "utils.h"
 
+/**
+ * @brief       Sums the two input arguments and returns the result.
+ *
+ * @param[in]   num1    First argument.
+ * @param[in]   num2    Second argument.
+ */
 int sum (int num1, int num2)
 {
     return (num1 + num2);
